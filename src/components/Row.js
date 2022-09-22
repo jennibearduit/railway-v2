0af7301e8f1cdc6e801642971941286ec755ba1b
jennibearduit <77873928+jennibearduit@users.noreply.github.com>
2022-09-23 02:08:13 +0800

    minor refactor

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,31 +2,27 @@ import Seat from './Seat';
 
 const Row = (props) => {
     const { travellers, rowId } = props;
-
-    
+    const takenSeatIds = travellers.map(t => t.id)
     const seatIds = Array
         .from({length: 5}, (_, i) => i+1)
         .map(num => {
             return (rowId === 1) ?  num : num+5
         })
-    const takenSeatIds = travellers.map(t => t.id)
+    
     const style = {
         width: `100%`,
         height: `100%`,
         display: `flex`,
         justifyContent: "center"
     }
+
     return (
         <div id="deck" style={style}>
-            {
-                seatIds.map(id => {
-                    const isAvailable = !takenSeatIds.includes(id);
-                    return (
-                        <Seat key={id} id={id} isAvailable={isAvailable}/>
-                    )
-                })
-
-            }
+            {seatIds.map(id => {
+                const isAvailable = !takenSeatIds.includes(id);
+                return (
+                    <Seat key={id} id={id} isAvailable={isAvailable}/>
+            )})}
         </div>
     )
 }
