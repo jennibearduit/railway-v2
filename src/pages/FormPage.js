b7fc2ac9dd1c7ab20120ab2ca43bf481afc028f2
jennibearduit <77873928+jennibearduit@users.noreply.github.com>
2022-09-22 19:45:25 +0800

    add form

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -2,10 +2,53 @@ import { PageState } from "./PageStateEnum"
 
 const FormPage = (props) => {
     const { pageState, onChangePage, travellers } = props;
+    const formStyle = {
+        margin: '50px 20px 20px 20px',
+        textAlign: 'center'
+    }
+    const inputStyle = {
+        boxSizing: 'border-box',
+        borderRadius: '5px',
+        margin: '20px 0px',
+        padding: '12px 20px',
+        width: '500px'
+    }
+
+    const allSeatIds = Array
+        .from({length: 10}, (_, i) => i+1)
+    const takenSeatIds = travellers.map(t => t.id)
+    const availableSeatIds = allSeatIds.filter(seat => !takenSeatIds.includes(seat))
+    console.log(availableSeatIds)
 
     return (
         <>
-            Form
+            <form style={formStyle}>
+                <label>Seat ID</label>
+                <br></br>
+                <select id="seatIds" name="seatIds" style={inputStyle}>
+                    {
+                        availableSeatIds.map(s => {
+                            return (
+                                <option value={s}>{s}</option>
+                            )
+                        })
+                    }
+                </select>
+                <br></br>
+                <label>Name</label>
+                <br></br>
+                <input type="text" style={inputStyle}></input>
+
+                <br></br><br></br>
+                <label>Phone Number</label>
+                <br></br>
+                <input type="text" style={inputStyle}></input>
+
+                <br></br><br></br>
+                <label>Passport</label>
+                <br></br>
+                <input type="text" style={inputStyle}></input>
+            </form>
         </>
     )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import { PageState } from './PageStateEnum'
 import Row from '../components/Row';
+import '../styles/home.css';
 
 const HomePage = (props) => {
     const { pageState, onChangePage:changePage, travellers, onAdd:addTraveller } = props;
@@ -13,6 +14,26 @@ const HomePage = (props) => {
         fontSize: '125%'
     }
 
+    const buttonStyle = {
+        alignItems: 'center',
+        backgroundColor: 'white',
+        border: 'solid',
+        borderRadius: '5px',
+        color: 'black',
+        cursor: 'pointer',
+        display: 'block',
+        fontFamily: 'inherit',
+        fontSize: '1.1rem',
+        margin: '5% auto',
+        padding: '1.3rem',
+        width: '25%'
+    }
+
+    const buttonClickHandler = (e) => {
+        e.preventDefault();
+        changePage(PageState.FORM);
+    }
+
     return (
         <>
             <div style={style}>
@@ -20,6 +41,7 @@ const HomePage = (props) => {
             </div>
             <Row travellers={travellers} rowId={1}/>
             <Row travellers={travellers} rowId={2}/>
+            <button className="reservationButton" style={buttonStyle} onClick={buttonClickHandler}>Add Booking</button>
         </>
     )
 }
